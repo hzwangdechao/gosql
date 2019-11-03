6d312dff25d8ed41aff1c4fafd2e67de561b0724
OhYee <oyohyee@oyohyee.com>
2019-11-03 21:27:35 +0800

    test: add testcases and cover 100% code(#master)

--- sql_test.go
+++ sql_test.go
@@ -12,13 +12,23 @@ type User struct {
 	Realname *Column
 }
 
-func NewUser() *User {
-	tb := NewTable("test", "user", "")
+func NewUser(alias ...string) *User {
+	const size = 3
+	as := make([]string, size)
+	for i := 0; i < size; i++ {
+		if len(alias) >= i+1 {
+			as[i] = alias[i]
+		} else {
+			as[i] = ""
+		}
+	}
+
+	tb := NewTable("test", "user", as[0])
 	return &User{
 		// SQL:      NewSQL().From(tb),
 		Table:    tb,
-		Username: NewColumn(tb, "username", "", ColumnTypeUnknow),
-		Realname: NewColumn(tb, "realname", "", ColumnTypeUnknow),
+		Username: NewColumn(tb, "username", as[1], ColumnTypeUnknown),
+		Realname: NewColumn(tb, "realname", as[2], ColumnTypeUnknown),
 	}
 }
 
@@ -53,6 +63,21 @@ func Test_main(t *testing.T) {
 			name:  "select in",
 			query: user.SQL().Select(user.Username, user.Realname).Where(op.In(user.Username, user.SQL().Select(user.Username).Where(op.Ne(user.Realname, "")))),
 			sql:   "SELECT `test`.`user`.`username`, `test`.`user`.`realname` FROM `test`.`user` WHERE (`test`.`user`.`username` IN (SELECT `test`.`user`.`username` FROM `test`.`user` WHERE (`test`.`user`.`realname` != '')));",
+		},
+		{
+			name:  "select max",
+			query: user.SQL().Select(user.Username, NewFunctionColumn("MAX(%s)", "max", user.Realname.String())),
+			sql:   "SELECT `test`.`user`.`username`, MAX(`test`.`user`.`realname`) AS `max` FROM `test`.`user`;",
+		},
+		{
+			name:  "select max without as",
+			query: user.SQL().Select(user.Username, NewFunctionColumn("MAX(%s)", "", user.Realname.String())),
+			sql:   "SELECT `test`.`user`.`username`, MAX(`test`.`user`.`realname`) FROM `test`.`user`;",
+		},
+		{
+			name:  "from table alias",
+			query: user.As("usr").SQL().Select(NewUser("usr", "name").Username),
+			sql:   "SELECT `usr`.`username` AS `name` FROM `test`.`user` AS `usr`;",
 		},
 	}
 
