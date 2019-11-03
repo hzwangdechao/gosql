ac1875c099f7c93296298174ae3e07b0a90f8af5
OhYee <oyohyee@oyohyee.com>
2019-11-03 21:26:34 +0800

    misc: add alias and prefix(#master)

--- table.go
+++ table.go
@@ -18,14 +18,25 @@ func NewTable(database string, name string, alias string) *Table {
 	}
 }
 
-func (tb *Table) SQL() *SQL{
+func (tb *Table) SQL() *SQL {
 	return NewSQL().From(tb)
+}
+
+func (tb *Table) As(alias string) *Table {
+	return NewTable(tb.Database, tb.Name, alias)
 }
 
 // String return from part of table
 func (tb *Table) String() string {
 	if tb.Alias != "" {
 		return fmt.Sprintf("`%s`.`%s` AS `%s`", tb.Database, tb.Name, tb.Alias)
+	}
+	return fmt.Sprintf("`%s`.`%s`", tb.Database, tb.Name)
+}
+
+func (tb *Table) Prefix() string {
+	if tb.Alias != "" {
+		return fmt.Sprintf("`%s`", tb.Alias)
 	}
 	return fmt.Sprintf("`%s`.`%s`", tb.Database, tb.Name)
 }
